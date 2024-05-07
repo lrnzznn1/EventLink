0354ddb762f04230b1d870ca98f4c1fa1322857b
lrnzznn1 <lorenzo.zanini@studenti.unitn.it>
2024-05-07 17:11:45 +0200

    Project

--- app/src/main/java/com/example/eventlink/MainActivity.kt
+++ app/src/main/java/com/example/eventlink/MainActivity.kt
@@ -28,6 +28,7 @@
         - Pagina Aiuto tutto
         - Aggiungere al db utenti, aziende, prenotazioni e preferiti
         - Variabile globale con id utente se 0 non loggato se n loggato
+        - Spostare tutte le pagine in altri file
 
 
 
