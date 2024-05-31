9e4b9b954fd534e25540c5310228c6e020cccb60
lrnzznn1 <lorenzo.zanini@studenti.unitn.it>
2024-05-31 14:07:49 +0200

    Project

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,8 +6,8 @@ plugins {
 
 buildscript {
     dependencies {
-        classpath("com.android.tools.build:gradle:8.4.1")
+        classpath("com.android.tools.build:gradle:8.3.2")
         classpath("com.google.android.libraries.mapsplatform.secrets-gradle-plugin:secrets-gradle-plugin:2.0.1")
-        classpath("com.google.gms:google-services:4.3.10")
+        classpath("com.google.gms:google-services:4.3.0")
     }
 }
