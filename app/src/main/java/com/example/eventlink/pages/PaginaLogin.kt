10a32106dd5d9248acc6f761f773e62c8f79383a
lrnzznn1 <lorenzo.zanini@studenti.unitn.it>
2024-06-02 15:50:15 +0200

    Project

--- app/src/main/java/com/example/eventlink/pages/PaginaLogin.kt
+++ app/src/main/java/com/example/eventlink/pages/PaginaLogin.kt
@@ -20,34 +20,38 @@ class PaginaLogin : Activity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login)
 
-        // Initialize UI elements
+        // Recupero i riferimenti agli elementi UI
         val emailField = findViewById<EditText>(R.id.editTextEmailLogin)
         val passwordField = findViewById<EditText>(R.id.editTextPasswordLogin)
         val buttonSignUp = findViewById<TextView>(R.id.registatiTesto)
         val buttonLogin = findViewById<Button>(R.id.buttonlogin)
         val buttonDimenticata = findViewById<TextView>(R.id.dimenticataTesto)
 
-        // Set click listeners
+        // Gestisco il click sul testo "Registrati"
         buttonSignUp.setOnClickListener {
             val intent = Intent(this@PaginaLogin, PaginaSignIn::class.java)
             startActivity(intent)
         }
 
+        // Gestisco il click sul testo "Password dimenticata"
         buttonDimenticata.setOnClickListener {
             val intent = Intent(this@PaginaLogin, PaginaDimenticata::class.java)
             startActivity(intent)
         }
 
+        // Gestisco il click sul pulsante "Login"
         buttonLogin.setOnClickListener {
-            // Perform login operation
             val animation = AnimationUtils.loadAnimation(this, R.anim.button_click_animation)
             buttonLogin.startAnimation(animation)
+
+            // Recupero i valori degli input email e password
             val email = emailField.text.toString()
             val password = passwordField.text.toString()
             var auth: Boolean
             auth = false
             var block = false
 
+            // Controllo se i campi email e password sono vuoti e mostro un alert se necessario
             if (email.isEmpty()) {
                 block = true
                 AlertDialog.Builder(this)
@@ -64,11 +68,13 @@ class PaginaLogin : Activity() {
                     .show()
             }
 
+            // Se i campi non sono vuoti, controllo le credenziali
             if(!block){
                 runBlocking {
                     auth = passwordCheck(email, password)
                 }
 
+                // Se le credenziali sono corrette, apro la PaginaProfilo
                 if(auth) {
                     val intent = Intent(this@PaginaLogin, PaginaProfilo::class.java)
                     intent.putExtra("email", email)
@@ -78,6 +84,7 @@ class PaginaLogin : Activity() {
                     finish()
                 }
                 else {
+                    // Se le credenziali sono errate, mostro un alert
                     val builder = AlertDialog.Builder(this)
                     builder.setTitle("Accesso Negato")
                     builder.setMessage("Email o Password errati!")
@@ -89,12 +96,14 @@ class PaginaLogin : Activity() {
         }
     }
 
-    //Suspended function to check if the provided password matches the one stored in the database for the given document ID.
+    // Funzione per verificare le credenziali
     private suspend fun passwordCheck(documentId: String, password: String): Boolean {
         return try {
+            // Recupero il documento dell'utente dal database
             val documentSnapshot = db.collection("Utenti").document(documentId).get().await()
             val documentData = documentSnapshot.data
             val documentPassword = documentData?.get("Password")
+            // Controllo se la password hashata corrisponde a quella memorizzata nel database
             documentPassword == hashString(password)
         }catch (e: Exception){
             false
